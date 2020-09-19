13f1b5d726c16e5662d55d842b85d827d6b13f42
Niels de Klerk <44940871+nikl881@users.noreply.github.com>
2020-09-19 10:21:48 +0200

    created controller methods for create and delete tasks

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -16,4 +16,31 @@ class ToDoListController extends AbstractController
             'controller_name' => 'ToDoListController',
         ]);
     }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function createTask()
+    {
+        exit('to do: create new task');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     * @param $id
+     */
+    public function switchStatus($id)
+    {
+        exit('to do: switch status of the task'. $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_task")
+
+     */
+    public function deleteTask($id)
+    {
+        exit('to do: delete task with the id of: '. $id );
+    }
+
 }
